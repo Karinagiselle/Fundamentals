dd6480f6112c101900349d706c0dbf3a53daa5de
Karina Blanco <karinagiselle.b@gmail.com>
2017-11-11 14:01:15 +0000

    Add test for Bubble Sort

--- src/test/java/SortingAlgorithms/BubbleSort.java
+++ src/test/java/SortingAlgorithms/BubbleSort.java
@@ -11,4 +11,28 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class BubbleSort {
 
+    @Test
+    public void bubbleSortTest() {
+
+        final int[] numbers = {5, 2, 6, 1, 3, 8, 9, 4};
+        final int[] expected = {1, 2, 3, 4, 5, 6, 8, 9};
+
+        bubbleSort(numbers);
+
+        assertThat(numbers).isEqualTo(expected);
+    }
+
+    private void bubbleSort(int[] numbers) {
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = 0; j < numbers.length - i - 1; j++) {
+
+                if (numbers[j] > numbers[j + 1]) {
+                    int aux = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = aux;
+                }
+            }
+        }
+    }
 }
